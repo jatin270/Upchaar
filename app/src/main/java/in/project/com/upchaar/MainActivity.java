3ff9425c15ab676d119a829d40c664a9b3ad4231
jatin270 <bansaljatin8@gmail.com>
2018-03-22 20:36:58 +0530

    Login Complete

--- app/src/main/java/in/project/com/upchaar/MainActivity.java
+++ app/src/main/java/in/project/com/upchaar/MainActivity.java
@@ -1,6 +1,8 @@
 package in.project.com.upchaar;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Constant.Constant;
+import Fragments.LoginFragment;
 import client.RestClient;
 import models.LoginUser;
 import models.User;
@@ -26,54 +29,22 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import services.UpchaarService;
 
+
 //https://infinum.co/the-capsized-eight/top-5-android-libraries-every-android-developer-should-know-about
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentTransaction trans;
+    private FragmentManager manager;
+    private LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manager=getSupportFragmentManager();
+        trans=manager.beginTransaction();
+        loginFragment= (LoginFragment)manager.findFragmentById(R.id.login_fragment);
 
-        UpchaarService libraryServiceAPI = RestClient.getClient();
-        final TextView textView= (TextView) findViewById(R.id.textView);
-
-        LoginUser user=new LoginUser();
-        user.setUsername("jatin11");
-        user.setPassword("qwerty@123");
-
-        Call<User> loginRequest = libraryServiceAPI.login(user);
-
-        loginRequest.enqueue(new Callback<User>() {
-        // calling doctor sign up
-        Intent PlayIntent=new Intent(MainActivity.this,DoctorSignUp.class);
-//        Log.d("xyxyxyx","" + x);
-//        PlayIntent.putExtra("x", x);
-//        PlayIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-
-
-
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                System.out.println(response.code());
-                if (response.isSuccessful()) {
-                    User user1 = response.body();
-
-                    if (user1 != null) {
-                        textView.setText("Login Successful");
-                    }
-                } else {
-                    textView.setText("Login Unsuccessful");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                textView.setText("Request Failed");
-                t.printStackTrace();
-            }
-
-        });
     }
 }
