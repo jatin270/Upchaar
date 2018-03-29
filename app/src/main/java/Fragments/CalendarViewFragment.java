8173aa2122f1edb973e3390d2cef03ede990fc37
bhavya14 <bhavya.gupta97@gmail.com>
2018-03-29 20:12:06 +0530

    adding calendar voew
    Signed-off-by: bhavya14 <bhavya.gupta97@gmail.com>

---
+++ app/src/main/java/Fragments/CalendarViewFragment.java
@@ -0,0 +1,28 @@
+package Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.project.com.upchaar.R;
+public class CalendarViewFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_calendar_view, container, false);
+
+        return view;
+    }
+
+}
