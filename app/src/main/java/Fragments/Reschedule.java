738707218a7c2d1de77795d0ef2f082c602d458b
bhavya14 <bhavya.gupta97@gmail.com>
2018-03-31 07:38:31 +0530

    conflicts resolved
    Signed-off-by: bhavya14 <bhavya.gupta97@gmail.com>

--- app/src/main/java/Fragments/Reschedule.java
+++ app/src/main/java/Fragments/Reschedule.java
@@ -6,4 +6,25 @@ package Fragments;
 
 public class Reschedule {
 
+    String doctor;
+    String hospital;
+    String time;
+    String date;
+    String token;
+
+    public Reschedule(String doctor,String hospital,String time,String date,String token){
+        this.doctor = doctor;
+        this.hospital = hospital;
+        this.time = time;
+        this.date = date;
+        this.token = token;
+    }
+
+    public String getDoctor(){return this.doctor ;}
+    public String getHospital(){return this.hospital ;}
+    public String getTime(){return this.time ;}
+    public String getDate(){return this.date ;}
+    public String getToken(){return this.token;}
+
+
 }
